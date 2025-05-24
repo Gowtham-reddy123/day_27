a2784c9c4a5979f6c6cdd1fd10ff1a9649e0b90a
Gowtham-reddy123 <reddyg026@gmail.com>
2025-05-24 17:05:32 +0530

    decorator_1

---
+++ decoators_5.py
@@ -0,0 +1,10 @@
+def Mo(a):
+        def inner(m):
+                sum=0
+                for x in range (1,101):
+                        if x%4==0 and x%5==0:
+                                sum+=x
+                print("sum of 4 and 5 multiples :",sum)
+        inner(a)
+Mo(1)
+                                                                                                                                                                        
